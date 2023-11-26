f34686fb738a8bf7d4fc10eaa2f542028a6684e1
Amol Grover <frextrite@gmail.com>
2023-11-26 20:37:22 +0530

    Implement get and set

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,14 @@ impl KeyValue {
     fn new() -> KeyValue {
         KeyValue { store: HashMap::new() }
     }
+
+    fn set(&mut self, key: String, val: String) -> Option<String> {
+        self.store.insert(key, val)
+    }
+
+    fn get(&self, key: String) -> Option<&String> {
+        self.store.get(&key)
+    }
 }
 
 #[cfg(test)]
@@ -18,5 +26,18 @@ mod tests {
     fn new_kv() {
         let kv = KeyValue::new();
         assert_eq!(kv.store.len(), 0);
+    }
+
+    #[test]
+    fn basics() {
+        let mut kv = KeyValue::new();
+
+        assert_eq!(kv.get("key".to_string()), None);
+
+        assert_eq!(kv.set("key".to_string(), "val".to_string()), None);
+        assert_eq!(kv.get("key".to_string()), Some("val".to_string()).as_ref());
+
+        assert_eq!(kv.set("key".to_string(), "val2".to_string()), Some("val".to_string()));
+        assert_eq!(kv.get("key".to_string()), Some("val2".to_string()).as_ref());
     }
 }
