f3ae2755135285c83fb9e498957f3f12f82e975d
Amol Grover <frextrite@gmail.com>
2023-11-30 22:22:47 +0530

    Implement uds client

--- src/kvs_client.rs
+++ src/kvs_client.rs
@@ -1,3 +1,8 @@
+#[cfg(unix)]
+use tokio::net::UnixStream;
+use tonic::transport::Endpoint;
+use tower::service_fn;
+
 pub mod kvs_proto_gen {
     tonic::include_proto!("kvs");
 }
@@ -5,9 +10,32 @@ pub mod kvs_proto_gen {
 use kvs_proto_gen::key_value_store_client::KeyValueStoreClient;
 use kvs_proto_gen::EchoRequest;
 
+#[cfg(windows)]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
    let mut client = KeyValueStoreClient::connect("http://127.0.0.1:50051/").await?;
+
+   let request = tonic::Request::new(EchoRequest {
+    message: "sphinx of black quartz, judge my vow".to_string(),
+   });
+
+   let response = client.echo_me(request).await?;
+
+   println!("INFO: Received response as \"{}\"", response.into_inner().message);
+
+   Ok(())
+}
+
+#[cfg(unix)]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+   let channel = Endpoint::try_from("http://127.0.0.1:50051/")?
+        .connect_with_connector(service_fn(|_| {
+            UnixStream::connect("\0/tmp/key_value_store")
+        }))
+        .await?;
+
+   let mut client = KeyValueStoreClient::new(channel);
 
    let request = tonic::Request::new(EchoRequest {
     message: "sphinx of black quartz, judge my vow".to_string(),
